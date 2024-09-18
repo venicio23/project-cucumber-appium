e8fbea94b47cec10d26af1e3215874af1f8fdecf
Marcos Venicio Viana <venicviana@gmail.com>
2024-09-18 11:30:25 -0300

    feat: Creating conditions for various platforms

--- src/test/java/com/test/Hooks.java
+++ src/test/java/com/test/Hooks.java
@@ -13,7 +13,14 @@ public class Hooks {
 
     private static AppiumDriver<?> driver;
 
+    public static String platform="";
+
     public static AppiumDriver<?> validateDriver() throws MalformedURLException {
+
+        platform = System.getProperty("plataforma");
+        System.out.println("Sua plataforma é: " + platform);
+
+        if (platform.equals("android")) {
 
         DesiredCapabilities cap = new DesiredCapabilities();
         File app = new File("apps/app-debug.apk");
@@ -21,8 +28,20 @@ public class Hooks {
         cap.setCapability("platformName", "Android");
         cap.setCapability("deviceName", "emulator-5554");
         driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-        return driver;
 
+        }else if (platform.equals("ios")) {
+
+            DesiredCapabilities cap = new DesiredCapabilities();
+            File app = new File("apps/app-debug.apk");
+            cap.setCapability("app", app.getAbsolutePath());
+            cap.setCapability("platformName", "Android");
+            cap.setCapability("deviceName", "emulator-5554");
+            driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        }else {
+            System.out.println("Plataforma não encontrada, utilize android ou ios");
+        }
+
+        return driver;
     }
 
     public static AppiumDriver<?> getDriver(){
